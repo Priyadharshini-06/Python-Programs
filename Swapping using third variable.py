544fc4217b230ee86688ceed0f0a57c30df3070d
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-18 10:57:16 +0530

    Add files via upload

---
+++ Swapping using third variable.py
@@ -0,0 +1,8 @@
+print("SWAPPING OF TWO NUMBERS USING THIRD VARIABLE")
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+print ("Before Swapping a=",a,"b=",b)
+t=a
+a=b
+b=t
+print("After Swapping a=",a,"b=",b)
