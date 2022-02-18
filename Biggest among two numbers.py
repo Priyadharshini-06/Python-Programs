e5118daf0b9363fe6c2d5e9e2ef988491a905939
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-18 11:21:34 +0530

    Add files via upload

---
+++ Biggest among two numbers.py
@@ -0,0 +1,7 @@
+print("BIGGEST AMONG TWO NUMBERS")
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+if(a>b):
+    print(a,"is bigger")
+else:
+     print(b,"is bigger")
